184647d282a0813bca040ab86d10c1d87f2921da
Shraddhadhaduk <shraddhadhaduk96@gmail.com>
2023-12-18 20:32:34 +0000

    week 6 project 2

---
+++ src/test/java/com/orangehrmlive/demo/runners/RunCukeTest.java
@@ -0,0 +1,19 @@
+package com.orangehrmlive.demo.runners;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/orangehrmlive/demo/steps",
+        plugin = {"html:target/cucumber-reports/cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        tags =  "@regression and not @smoke"
+)
+public class RunCukeTest extends AbstractTestNGCucumberTests {
+
+
+
+
+}
